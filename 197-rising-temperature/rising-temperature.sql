010de30f11bfec12b5e7bbcb4540fc43aec472fe
Abhay Vishe <141409692+Abhayvishe@users.noreply.github.com>
2026-01-31 13:15:48 +0530

    Time: 375 ms (79.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,9 @@
+SELECT w1.id
+FROM Weather w1
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather w2
+    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+      AND w2.temperature < w1.temperature
+);
+
